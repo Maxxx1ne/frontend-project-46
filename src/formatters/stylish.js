8ac03d43ee5c98678ecf48a87c8ef53d0d338cbd
Maxxxine <valedol2280@gmail.com>
2025-11-14 04:17:45 +0300

    fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,10 +3,8 @@ import _ from 'lodash';
 const formatValue = (value, depth) => {
   if (_.isPlainObject(value)) {
     const indent = ' '.repeat(depth * 4);
-    const bracketIndent = ' '.repeat((depth - 1) * 4);
-    const lines = Object.entries(value).map(([key, val]) => {
-      return `${indent}  ${key}: ${formatValue(val, depth + 1)}`;
-    });
+    const bracketIndent = ' '.repeat(depth * 4 - 4);
+    const lines = Object.entries(value).map(([key, val]) => `${indent}${key}: ${formatValue(val, depth + 1)}`);
     return ['{', ...lines, `${bracketIndent}}`].join('\n');
   }
 
@@ -17,20 +15,20 @@ const formatValue = (value, depth) => {
 
 const stylish = (tree, depth = 1) => {
   const indent = ' '.repeat(depth * 4 - 2);
-  const bracketIndent = ' '.repeat((depth - 1) * 4);
+  const bracketIndent = ' '.repeat(depth * 4 - 4);
 
   const lines = tree.map((node) => {
     switch (node.type) {
       case 'added':
-        return `${indent}+ ${node.key}: ${formatValue(node.value, depth)}`;
+        return `${indent}+ ${node.key}: ${formatValue(node.value, depth + 1)}`;
       case 'removed':
-        return `${indent}- ${node.key}: ${formatValue(node.value, depth)}`;
+        return `${indent}- ${node.key}: ${formatValue(node.value, depth + 1)}`;
       case 'unchanged':
-        return `${indent}  ${node.key}: ${formatValue(node.value, depth)}`;
+        return `${indent}  ${node.key}: ${formatValue(node.value, depth + 1)}`;
       case 'changed':
         return [
-          `${indent}- ${node.key}: ${formatValue(node.value1, depth)}`,
-          `${indent}+ ${node.key}: ${formatValue(node.value2, depth)}`,
+          `${indent}- ${node.key}: ${formatValue(node.value1, depth + 1)}`,
+          `${indent}+ ${node.key}: ${formatValue(node.value2, depth + 1)}`,
         ].join('\n');
       case 'nested':
         return `${indent}  ${node.key}: ${stylish(node.children, depth + 1)}`;
